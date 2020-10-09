9af2e4b5d259ebd6f143b15d375f6bfa925fe8c9
shadowfist04 <72393544+shadowfist04@users.noreply.github.com>
2020-10-09 11:58:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Peer-graded Assignment: Bash, Make, Git, and GitHub
+friday oct 9 2020 12:00pm
+   
---
+++ makefile.mk
@@ -0,0 +1,5 @@
+touch README.md
+echo "# Peer-graded Assignment: Bash, Make, Git, and GitHub" > README.md
+date >> README.md
+echo "    " >> README.md
+wc -l < guessinggame.sh >> README.md
